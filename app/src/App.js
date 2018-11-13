32910efd54081a12ca642b51bcbffa975124c735
Bill Pond <william.e.pond@gmail.com>
2018-11-13 14:59:04 -0500

    Introducing Aphrodite

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import {StyleSheet, css} from 'aphrodite';
 import './App.css';
+
+const styles = StyleSheet.create({
+  welcome: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white'
+  }
+});
+
+const name = css(styles.welcome);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className={name}>
           <h1> Welcome to Bill Pond.com!</h1>
         </header>
       </div>
