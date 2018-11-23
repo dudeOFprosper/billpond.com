9641b2fdd9ec75bdfe52216b5c25e4b42deb5228
Bill Pond <william.e.pond@gmail.com>
2018-11-23 14:55:17 -0500

    Updates

---
+++ precache-manifest.f51087b5c0e8d253e232304d1b02c96b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc800aa2dc0d6a74c24914ec890fd09d",
+    "url": "/billpond.com/static/media/tom.fc800aa2.jpg"
+  },
+  {
+    "revision": "415f309bf2bbf83278da",
+    "url": "/billpond.com/static/css/main.b8a46c8d.chunk.css"
+  },
+  {
+    "revision": "bf4c64ac9f88b23f71d7",
+    "url": "/billpond.com/static/js/1.bf4c64ac.chunk.js"
+  },
+  {
+    "revision": "6fd9447f3004097a96db",
+    "url": "/billpond.com/static/js/runtime~main.6fd9447f.js"
+  },
+  {
+    "revision": "2c692054a9fc6ba12c9c9f64a2fbefdd",
+    "url": "/billpond.com/static/media/myspace.2c692054.jpg"
+  },
+  {
+    "revision": "8d46739fc68632c3b5f752ce4ff64d09",
+    "url": "/billpond.com/static/media/profile.8d46739f.jpg"
+  },
+  {
+    "revision": "415f309bf2bbf83278da",
+    "url": "/billpond.com/static/js/main.415f309b.chunk.js"
+  },
+  {
+    "revision": "405c2ba40edc78c04dacbd5621f56f72",
+    "url": "/billpond.com/static/media/kyle.405c2ba4.jpg"
+  },
+  {
+    "revision": "a00c648f38182d1767acb15da18ebf79",
+    "url": "/billpond.com/static/media/jordan.a00c648f.jpg"
+  },
+  {
+    "revision": "944371e05f23ea8b912f21e093064fc1",
+    "url": "/billpond.com/static/media/max.944371e0.jpg"
+  },
+  {
+    "revision": "c43a4e72d4537a18f97c0b02bccf6915",
+    "url": "/billpond.com/static/media/ethan.c43a4e72.jpg"
+  },
+  {
+    "revision": "0c9984985f056e801e4686be4a6a6615",
+    "url": "/billpond.com/static/media/sola.0c998498.jpg"
+  },
+  {
+    "revision": "f9ee609ab3158f8142ba5fc2dca789c3",
+    "url": "/billpond.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.e9f4d3578d651b8a014f33e24878ef6c.js"
+  "/billpond.com/precache-manifest.f51087b5c0e8d253e232304d1b02c96b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/billpond.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
