a168b3e917bb8c9c02f8b40c54b97c6b6041fc12
Bill Pond <william.e.pond@gmail.com>
2019-01-23 16:34:25 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/billpond.com/precache-manifest.2e052ab35a44d7ecf0e17b263e6df719.js"
+  "/billpond.com/precache-manifest.791c27133ebca79f14155b2032253ef6.js"
 );
 
 workbox.clientsClaim();
